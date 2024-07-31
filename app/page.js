acd0b81be81ac24aef10c59e4ec1338e2bd7481e
Sidratul Muntaha Sara <sidratul.s@outlook.com>
2024-07-31 06:56:14 +0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import { collection, query, setDoc, deleteDoc, doc, getDoc, getDocs } from 'fire
 
 export default function Home() {
   const [inventory, setInventory] = useState([])
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(true)
   const [item, setItemName] = useState('')
 
   const updateInventory = async () => {
