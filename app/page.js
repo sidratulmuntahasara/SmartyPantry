daf5cb8cc39e766cd2fb3c4f25b60f55967d16b1
Sidratul Muntaha Sara <sidratul.s@outlook.com>
2024-08-01 19:35:52 +0400

    updated

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import { useState, useEffect } from 'react'
 import React from 'react'
 import Image from 'next/image'
-import { Box, Modal, Typography, Stack, TextField, Button, AppBar } from '@mui/material'
+import { Box, Modal, Typography, Stack, TextField, Button, AppBar, Fab } from '@mui/material'
 import { firestore } from '../firebase'
 import { collection, query, setDoc, deleteDoc, doc, getDoc, getDocs } from 'firebase/firestore'
 // import { ResponsiveAppBar } from "./appbar";
@@ -80,52 +80,22 @@ export default function Home() {
     justifyContent="space-between"
     alignItems="start"
     gap={2} className="bg-black">
-      <AppBar position="sticky" className='bg-transparent border-b border-white border-opacity-25 flex content-evenly justify-items-center flex-row place-content-evenly padding-500'> 
-      <Image></Image> 
-      <Typography className='text-3xl align-middle'> Pantry Pal </Typography>
-      <Button >Green</Button>
+      <AppBar position="sticky"  className='bg-transparent border-b border-white border-opacity-25 flex content-evenly justify-items-center flex-row place-content-evenly py-3'> 
+      <Image src="/favicon.ico" width={80} height={80} alt="logo Smarty Pantry"/> 
+      <Typography className='text-3xl text-center items-center flex text-pink-200'> <Typography className='text-3xl text-pink-500 text-center items-center flex'>S</Typography>marty&nbsp; <Typography className='text-3xl text-pink-500 text-center items-center flex'>P</Typography>antry </Typography>
+      <Button className="text-pink-300 font-bold text-base px-3 ">Get Recipes ✨</Button>
       </AppBar>
       {/* <ResponsiveAppBar/> */}
       <Box width="100vw" height="100vh" display="flex" flexDirection={"column"} justifyContent="center" alignItems="center" gap={2}>
-      {/* <Modal open={open} onClose={handleClose}>
-        <Box
-        position={'absolute'}
-        top={"50%"}
-        left={"50%"}
-        width={400}
-        bgcolor={"white"}
-        border={"2px solid #eee"}
-        boxShadow={24}
-        p={4}
-        display={"flex"}
-        flexDirection={'column'}
-        gap={3}
-        sx={{
-          transform: 'translate(-50%, -50%)',
-        }}
-        >
-          <Typography variant='h6'>Add Item</Typography>
-          <Stack width="100%" direction="row" spacing={2}>
-            <TextField 
-            variant='outlined' 
-            fullWidth 
-            value={item} 
-            onChange={(e) => {
-              setItemName(e.target.value)
-            } } />
-            <Button
-            variant='outlined'
-            onClick={() => {addItem(item)
-              setItemName('')
-              handleClose()
-            }}
-            >
-              ADD
-            </Button>
-          </Stack>
+      <Modal open={open} onClose={handleClose}>
+        <Box position={'absolute'} top={"50%"} left={"50%"} border={"2px solid #eee"} width={650} boxShadow={24} p={4} display={"flex"} flexDirection={'column'} gap={3} sx={{  transform: 'translate(-50%, -50%)', }} className=" bg-gradient-to-br from-pink-200 to-purple-900">
+          <Typography variant='h5' className="text-center font-bold text-fuchsia-950">WELCOME TO SMARTY PANTRY!</Typography>
+          <Typography className="flex-wrap text-center text-white">Hello There! I am MissAI, the Creator of SmartyPantry. Smarty Pantry comes with an immersive blend of AI, UI, Frontend and Backend, all with the support of Next.js, React, Firebase, Material UI, Tailwind CSS, Daisy UI, Open AI API, & Llama 3.1 API</Typography>
+          <Typography className="flex-wrap text-center text-white">Play around and do whatever, the park is yours!</Typography>
+          <Button variant='contained' onClick={handleClose} className="bg-black hover:bg-pink-500">Close</Button>
         </Box>
-      </Modal> */}
-      <Button variant='contained' onClick={() => {handleOpen()}}>Add New Item</Button>
+      </Modal>
+      <Fab variant="extended" onClick={() => {handleOpen()}} className="absolute font-bold bottom-14 right-14 bg-pink-700 shadow-red-500/50 shadow-md text-white hover:text-pink-700">Add New Item</Fab>
       <Box border="1px solid #eee">
         <Box width={"800px"} height={"100px"} bgcolor={"#ADD8E6"} display={"flex"} alignItems={"center"} justifyContent={"center"}>
             <Typography variant='h2' color={'#333'} >Inventory Items</Typography>
